a5d4531bf73fd0f25f58ef62efc1588ff10c74c9
Tiffany Crockett <crockett.tiff@gmail.com>
2020-12-03 20:46:19 -0700

    html, css, quiz questions, variables on js

---
+++ questions.js
@@ -0,0 +1,37 @@
+//  create an array of questions - question index 0,1,2,3,4
+ var questions = [{
+    "question": "What is HTML?",
+    "option1": "The syntax for JavaScript",
+    "option2": "Hyper Text Markup Library",
+    "option3": "The language used to style a Web page",
+    "option4": "None of the Above",
+    "answer": "4"
+ }, {
+     "question": "Which is NOT a part of the CSS box model?",
+     "option1": "Content",
+     "option2": "Border",
+     "option3": "Table",
+     "option4": "Margin",
+     "answer": "3"
+ }, {
+     "question": "A block of JavaScript code executed when \"called\" for:",
+     "option1": "Script",
+     "option2": "Function",
+     "option3": "For Loop",
+     "option4": "String",
+     "answer": "2"
+ }, {
+     "question": "In JavaScript, this is used to return random integers:",
+     "option1": "Math.round",
+     "option2": "Math.random",
+     "option3": "Math.sqrt",
+     "option4": "Math.floor",
+     "answer": "4"
+}, {
+    "question": "This is used to store multiple values in a single variable:",
+    "option1":  "Array",
+    "option2":  "URL",
+    "option3":  "Event",
+    "option4":  "Number",
+    "answer":   "1"
+}];
---
+++ script.js
@@ -0,0 +1,21 @@
+var start = document.querySelector('.start'); 
+var quiz = document.querySlector('.quiz');  
+var viewScores = document.querySelector('#viewScores');
+var myScore = document.querySelector('#myScore');
+var questionEl = document.querySelector('#question');
+var opt1 = document.querySelector('#opt1');
+var opt2 = document.querySelector('#opt2');
+var opt3 = document.querySelector('#opt3');
+var opt4 = document.querySelector('#opt4'); 
+var comment = document.querySelector('#comment'); 
+var gameOver = document.querySelector('.gameOver');
+var result = document.querySelector('#result'); 
+var highScore = document.querySelector('.highScore');
+var playerList = document.querySelector('#playerList'); 
+var startButton = document.querySelector('#startButton');
+var nexttButton = document.querySelector('#nextButton');
+var submitButton = document.querySelector('#submitButton');
+var resetButton = document.querySelector('#resetButton'); 
+var clearButton = document.querySelector('#clearStorageButton');
+
+ 
