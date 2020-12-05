c04bfa8f2864823bf40da88eb9fca00665c3515c
Tiffany Crockett <crockett.tiff@gmail.com>
2020-12-05 01:09:47 -0700

    init, storage, renderList

--- questions.js
+++ questions.js
@@ -1,34 +1,34 @@
-//  create an array of questions - question index 0,1,2,3,4
- var questions = [{
-    "question": "What is HTML?",
-    "option1": "The syntax for JavaScript",
-    "option2": "Hyper Text Markup Library",
-    "option3": "The language used to style a Web page",
+//  create an array of questions
+var questions = [{
+    "question": "What is JavaScript?",
+    "option1": "The syntax for HTML",
+    "option2": "A library of code used for web pages",
+    "option3": "The language used to style a web page",
     "option4": "None of the Above",
     "answer": "4"
  }, {
-     "question": "Which is NOT a part of the CSS box model?",
-     "option1": "Content",
-     "option2": "Border",
-     "option3": "Table",
-     "option4": "Margin",
+     "question": "What was JavaScript's first name?",
+     "option1": "LiveScript",
+     "option2": "Mona",
+     "option3": "Mocha",
+     "option4": "None of the Above",
      "answer": "3"
  }, {
-     "question": "A block of JavaScript code executed when \"called\" for:",
+     "question": "A block of code executed when \"called\" for?",
      "option1": "Script",
      "option2": "Function",
      "option3": "For Loop",
      "option4": "String",
      "answer": "2"
  }, {
-     "question": "In JavaScript, this is used to return random integers:",
+     "question": "What returns random integers?",
      "option1": "Math.round",
      "option2": "Math.random",
      "option3": "Math.sqrt",
      "option4": "Math.floor",
      "answer": "4"
 }, {
-    "question": "This is used to store multiple values in a single variable:",
+    "question": "Stores multiple values in a single variable?",
     "option1":  "Array",
     "option2":  "URL",
     "option3":  "Event",
--- script.js
+++ script.js
@@ -1,21 +1,137 @@
-var start = document.querySelector('.start'); 
-var quiz = document.querySlector('.quiz');  
-var viewScores = document.querySelector('#viewScores');
-var myScore = document.querySelector('#myScore');
-var questionEl = document.querySelector('#question');
-var opt1 = document.querySelector('#opt1');
-var opt2 = document.querySelector('#opt2');
-var opt3 = document.querySelector('#opt3');
-var opt4 = document.querySelector('#opt4'); 
-var comment = document.querySelector('#comment'); 
-var gameOver = document.querySelector('.gameOver');
-var result = document.querySelector('#result'); 
-var highScore = document.querySelector('.highScore');
-var playerList = document.querySelector('#playerList'); 
-var startButton = document.querySelector('#startButton');
-var nexttButton = document.querySelector('#nextButton');
-var submitButton = document.querySelector('#submitButton');
-var resetButton = document.querySelector('#resetButton'); 
-var clearButton = document.querySelector('#clearStorageButton');
+var currentQuestion = 0;
+var score = 0;
+var totalQuestions = questions.length; 
+var startContainerEl = document.getElementById('start'); 
+var quizContainerEl = document.getElementById('quiz');  
+var gameOverContainerEl = document.getElementById('gameOver'); 
+var highScoreContainerEl = document.getElementById('highScore');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4'); 
 
- 
+var myScore = document.getElementById('myScore');
+var timer = document.getElementById('timer');
+var comment = document.getElementById('comment'); 
+var result = document.getElementById('result');  
+var startButton = document.getElementById('startButton');
+var nexttButton = document.getElementById('nextButton');
+var submitButton = document.getElementById('submitButton');
+var resetButton = document.getElementById('resetButton'); 
+var clearButton = document.getElementById('clearStorageButton'); 
+
+var players = [];
+var storedPlayers = [];
+
+function init() {
+    var playerString = localStorage.getItem('storedPlayers');
+    if(playerString !==null || playerString != '') {
+        storedPlayers = JSON.parse(playerString);
+        savedPlayers = storedPlayers;
+        renderPlayers(); 
+    }
+} 
+
+function storedPlayers() {
+    localStorage.setItem('savedPlayers', JSON.stringify(players));
+} 
+
+function renderPlayersList() { 
+    gameOverContainerEl.style.display = 'none';
+     highScoreContainerEl.style.display =  ''; 
+
+    playersList.textContent = ''; 
+    for(var i = 0; i < players.length; i++) {
+        players = players[i]; 
+
+        var li = document.createElement('li');
+        li.textContent = playerInfo;
+        playersList.appendChild(li);
+    } 
+    
+        submitButton.addEventListener('submit', function (event) {
+            event.preventDefault(); 
+
+        playerInitials = playerInput.value.trim();
+        if(playersInitials === '') {
+            return;
+        }
+        
+        players.push(playerInitials + score + 'points');
+        playerInitials.value = '';
+
+        storedPlayers();
+        renderPlayersList();
+        })
+    }  
+
+init();
+
+startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', loadNextQuestion);
+
+function startQuiz() { 
+    startContainerEl.style.display = 'none'; 
+    quizContainerEl.style.display = '';
+    loadQuestion(currentQuestion);
+}
+// // get questions from the question array
+function loadQuestion(questionIndex) {  
+    var questn = questions[questionIndex];  
+    questionEl.textContent = questn.question;
+    opt1.textContent = questn.option1;
+    opt2.textContent = questn.option2;
+    opt3.textContent = questn.option3;
+    opt4.textContent = questn.option4; 
+ }  
+
+function loadNextQuestion() { 
+    //player must choose an option for quiz to proceed 
+    var playerChoice = document.querySelector('input[type=radio]:checked');
+    
+    var playerAnswer = playerChoice.value;
+        if(questions[currentQuestion].answer === playerAnswer) {
+            score += 10;
+            myScore.textContent = 'Score: ' + score;
+            comment.style.display = '';
+            comment.textContent = 'Your answer is correct!'; 
+            } else {   
+            comment.style.display = '';
+            comment.textContent = 'Wrong answer!'; 
+        } 
+        playerChoice.checked = false;
+        currentQuestion++;
+        if(currentQuestion === totalQuestions) {
+            quizContainerEl.style.display = 'none';
+            gameOverContainerEl.style.display = '';
+            result.textContent = 'Your Score: ' + score;
+            return;
+        }
+        loadQuestion(currentQuestion); 
+}
+
+renderPlayersList();
+
+// var viewScoresEl = document.getElementById('viewScores');
+// viewHighScoresEl.addEventListener('click', getPlayers);
+// function getStoredPlayers() { 
+    
+//     quizContainerEl.style.display = 'none';
+//     highScoreContainerEl.style.display = '';
+//     localStorage.getItem('storedPlayers');
+// }
+
+// function startOver () { 
+// 	highScore.style.display = "none";
+// 	start.style.display = "block";
+// 	// clear playerList
+// }
+
+// function clearStorage() {
+// // clear all stored scores from local storage
+// }
+
+// resetButton.addEventListener('click', startOver);
+// clearButton.addEventListener('click', removeScores);
+
