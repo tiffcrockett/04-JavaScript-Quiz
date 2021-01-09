2dfab1f14578c8a06258a8694e01a493394d8ef0
Tiffany Crockett <61260599+tiffcrockett@users.noreply.github.com>
2021-01-09 15:36:21 -0700

    Update questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 //  create an array of questions
 var questions = [{
     "question": "What is JavaScript?",
-    "opt1": "The syntax for HTML",
-    "opt2": "A library of code used for web pages",
-    "opt3": "The language used to style a web page",
-    "opt4": "None of the Above",
+    "option1": "The syntax for HTML",
+    "option2": "A library of code used for web pages",
+    "option3": "The language used to style a web page",
+    "option4": "None of the Above",
     "answer": "4"
  }, {
      "question": "What was JavaScript's first name?",
-     "opt1": "LiveScript",
-     "opt2": "Mona",
-     "opt3": "Mocha",
-     "opt4": "None of the Above",
+     "option1": "LiveScript",
+     "option2": "Mona",
+     "option3": "Mocha",
+     "option4": "None of the Above",
      "answer": "3"
  }, {
      "question": "A block of code executed when \"called\" for?",
